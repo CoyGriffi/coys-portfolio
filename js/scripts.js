8b4388db05af0ad64b19c60d0c817ca6e276292b
coygriffi <coygriffi@gmail.com>
2021-11-01 12:07:19 -0700

    add clickable div that opens OL on about me

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#futurelistshowing").fadeToggle();
+    $("#futurelisthidden").fadeToggle();
+  });  
+});
+
+$(document).ready(function() {
+  $(".clickable2").click(function() {
+    $("#likesshowing").fadeToggle();
+    $("#likeshidden").fadeToggle();
+  });  
+});
