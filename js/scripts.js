2f130430fd931d486ffe1818e5420ea1259d6f0c
coygriffi <coygriffi@gmail.com>
2021-11-01 14:48:16 -0700

    add fade in function in multiple locations on index page

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,23 @@ $(document).ready(function() {
     $("#likeshidden").fadeToggle();
   });  
 });
+
+$(document).ready(function() {
+  $(".top").fadeIn(5000);
+});
+
+$(document).ready(function() {
+  $(".p1").fadeIn(8000);
+});
+
+$(document).ready(function() {
+  $(".p2").fadeIn(10000);
+});
+
+$(document).ready(function() {
+  $(".top").fadeIn(14000);
+});
+
+$(document).ready(function() {
+  $(".top").fadeIn(14000);
+});
